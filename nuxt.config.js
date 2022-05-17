7f23e1d17c9c7d2c0dce1e6d090353096ce4e675
salunaalavi <salunaalavi@gmail.com>
2022-05-17 19:55:42 +0700

    ready to deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,7 +67,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: process.env.NODE_ENV === '/' ? 'http://localhost:3000'
+    : 'https://my-domain.netlify.app',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
