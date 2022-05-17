3857bd9efce0510bbf0a7b94c57bae4fbcef7fb5
salunaalavi <salunaalavi@gmail.com>
2022-05-17 21:58:42 +0700

    ready to deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,8 +67,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.NODE_ENV === '/' ? 'http://localhost:3000'
-    : 'https://bike-rental-saluna.netlify.app',
+    baseURL: '/',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
